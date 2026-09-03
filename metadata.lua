e8dcf029f92f31c2522759dad7361aa407b9b66a
Frederico A. C. Azevedo <fredericoacazevedo@gmail.com>
2026-09-02 21:17:02 -0400

    TEMPORARY: split the 26s top-up block across its three phases

--- metadata.lua
+++ metadata.lua
@@ -13,7 +13,7 @@ return PlaceObj('ModDef', {
 	},
 	'id', "SuperBigMap",
 	'author', "fredware",
-	'version', 1039,
+	'version', 1040,
 	'lua_revision', 350453,
 	'saved_with_revision', 392284,
 	'code', {
