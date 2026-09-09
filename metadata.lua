10f34e3b2b93214fb4530f7f345661840127abb7
Frederico A. C. Azevedo <fredericoacazevedo@gmail.com>
2026-09-09 18:05:25 -0400

    Revert "Cache immutable rock capture geometry and reject impossible rays"
    This reverts commit 3c03de43bd00771d99026701d0095b6993ad5525.

--- metadata.lua
+++ metadata.lua
@@ -13,7 +13,7 @@ return PlaceObj('ModDef', {
 	},
 	'id', "SuperBigMap",
 	'author', "fredware",
-	'version', 967,
+	'version', 966,
 	'lua_revision', 350453,
 	'saved_with_revision', 392284,
 	'code', {
