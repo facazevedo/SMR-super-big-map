97a70205b120ef87bb0abc7f39388d6e0a7d2910
Frederico A. C. Azevedo <fredericoacazevedo@gmail.com>
2026-09-24 17:03:42 -0400

    TEMPORARY: release timing autostart + census on top of the marker fix (version 1108)
    Re-applies the temporary Mars.exe timing build (reverting 4e8cf6d) on top of efec17e to verify the release decor top-up. To be reverted after the run.

--- metadata.lua
+++ metadata.lua
@@ -14,7 +14,7 @@ return PlaceObj('ModDef', {
 	},
 	'id', "SuperBigMap",
 	'author', "fredware",
-	'version', 1107,
+	'version', 1108,
 	'lua_revision', 350453,
 	'saved_with_revision', 403908,
 	'code', {
